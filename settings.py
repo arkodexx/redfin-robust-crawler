629c2795f7e4664dacc2dc4eba4d9cf4f0d5151e
Arko <tiartee3@gmail.com>
2026-02-02 22:12:36 +0100

    Clean up deprecated settings in settings.py
    Removed deprecated download handlers and reactor settings.

--- settings.py
+++ settings.py
@@ -88,10 +88,3 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
-
-DOWNLOAD_HANDLERS = {
-    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-}
-
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
